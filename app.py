8d81ea892b295f2c25c57dcf10c4d440e46a9ea3
Nabdeep Patel <132994846+nabdeep-patel@users.noreply.github.com>
2024-03-17 11:47:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,23 @@ def get_gemini_response(input,image):
     return response.text
     
 def main():
+    st.sidebar.header("Navigation")
+
+    # Sidebar navigation links with bullets
+    st.sidebar.markdown("- [GitHub Repo](https://github.com/nabdeep-patel/image-qa/)")
+    st.sidebar.write("---")
+    
+    # Connect with me section
+    st.sidebar.markdown("Connect with me:")
+    github_link = "[![GitHub](https://img.shields.io/badge/GitHub-Profile-blue?style=flat-square&logo=github)](https://github.com/nabdeep-patel)"
+    linkedin_link = "[![LinkedIn](https://img.shields.io/badge/LinkedIn-Profile-blue?style=flat-square&logo=linkedin)](https://www.linkedin.com/in/nabdeeppatel)"
+    website_link = "[![Website](https://img.shields.io/badge/Personal-Website-blue?style=flat-square&logo=chrome)](https://linktr.ee/nabdeeppatel/store)"
+    email_link = "[![Email](https://img.shields.io/badge/Google-Mail-blue?style=flat-square&logo=gmail)](mailto:nabdeeppatel@gmail.com)"
+    
+    st.sidebar.markdown(github_link + " " + linkedin_link + " " + website_link + " " + email_link)
+    st.sidebar.markdown("Created by Nabdeep Patel")
+
+    
     st.title("Webcam Image Capture")
     st.write("Capture an Image from Webcam or Upload an Image")
 
